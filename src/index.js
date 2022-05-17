607d73f0391d554d3681a828b8a5d3065cb9254d
joanmontiel4 <joanmontiel@gmail.com>
2022-05-17 15:41:52 +0200

    Initial tryouts html doc

--- src/index.js
+++ src/index.js
@@ -13,6 +13,31 @@ const newBoard = new gameBoard(10, 10);
 // setTimeout(clearInterval, 10000, intervalID);
 ////////////////////////////////////////////////////////////
 
+// document.body.style.backgroundColor = 'red';
+
 const boardHTML = newBoard.generateBoardString();
 // console.log(boardHTML);
 document.main.innerHTML = boardHTML;
+// document.body.style.backgroundColor = 'red';
+
+// let mainDoc = querySelector('main');
+// mainDoc.style.backgroundColor = 'black';
+// document.body.style.backgroundColor = 'grey';
+
+// let cellHTML = document.querySelector(".cell");
+// cellHTML.style.
+
+// Create our stylesheet
+
+// var style = document.createElement('style');
+// style.innerHTML = `.cell {
+// 		color: grey;
+// 		background-color: #e5e5e5;
+// 		height: 100px;
+// 	}`;
+
+// // Get the first script tag
+// var ref = document.querySelector('script');
+
+// // Insert our new styles before the first script tag
+// ref.parentNode.insertBefore(style, ref);
